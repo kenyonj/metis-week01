203209db3bd848c8d14e38bc3cd71b63099c1546
Justin Kenyon <kenyonj@gmail.com>
2014-02-26 17:27:56 -0500

    add music files

---
+++ day03/music/music.rb
@@ -0,0 +1,10 @@
+require 'csv'
+require 'pp'
+
+database = Hash.new { |hash,key| hash[key] = [] }
+CSV.foreach("music.csv", {headers: true}) do |music_list|
+  artist = music_list["Artist"]
+  song_name = music_list["Name"]
+  database[artist] << song_name
+end
+pp database
