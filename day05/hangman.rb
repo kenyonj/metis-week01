a79003288e769a68fd7b5926eb1f4d74bdcc70a0
Justin Kenyon <kenyonj@gmail.com>
2014-03-01 17:22:21 -0500

    complete hangman, fix all errors

--- day05/hangman.rb
+++ day05/hangman.rb
@@ -1,22 +1,23 @@
-require 'pp'
 dictionary = []
-#command_line_args = ""#ARGV.strip
+command_line_args = ARGV.shift
 amount_of_guesses_allowed = 6
 
-def read_dictionary(dictionary)#, command_line_args)
-#  if command_line_args[0].to_s != ""
-  IO.foreach("/usr/share/dict/words" ) { |word| dictionary << word.chomp.downcase }
-# else
-#   puts "You must specifiy a dictionary file. Example: ruby hangman.rb /usr/share/dict/words"
-# end
+def read_dictionary(dictionary, command_line_args)
+  if command_line_args
+    IO.foreach(command_line_args) { |word| dictionary << word.chomp.downcase }
+  else
+    IO.foreach("/usr/share/dict/words") { |word| dictionary << word.chomp.downcase }
+  end
 end
 
-read_dictionary(dictionary)#, command_line_args)
+read_dictionary(dictionary, command_line_args)
 chosen_word = dictionary.sample
 correct_guesses = []
 winner = nil
 bad_guesses = []
 chosen_letters = chosen_word.split(//)
+
+amount_of_guesses_total = 0
 
 puts "Welcome to Hangman!"
 puts "You have #{amount_of_guesses_allowed} guesses left."
@@ -24,20 +25,34 @@ puts "_" * chosen_letters.length
 print "> "
 letter_guess = gets.chomp
 
-until winner do
+until winner || amount_of_guesses_allowed == 0 do
   if chosen_letters.any? { |letter| letter == letter_guess }
     correct_guesses << letter_guess
     correct_guesses.uniq!
+    letter_guess = ""
+    amount_of_guesses_total = amount_of_guesses_total + 1
   else
     puts "Sorry, that letter does not exist in the word."
     bad_guesses << letter_guess
     letter_guess = ""
     amount_of_guesses_allowed = amount_of_guesses_allowed - 1
+    amount_of_guesses_total = amount_of_guesses_total + 1
+  end
+
+  if chosen_letters.sort.uniq == correct_guesses.sort.uniq
+    winner = true
+    puts "Congratulations! You have guessed the word #{chosen_word} correctly in only #{amount_of_guesses_total} guesses."
+    break
+  elsif amount_of_guesses_allowed == 0
+    puts "Sorry, you have no guesses left. Game over. The word was #{chosen_word}"
+    break
   end
 
   puts "You have #{amount_of_guesses_allowed} guesses left."
-  print "You have already guessed these letters:"
-  pp bad_guesses.sort
+  print "You have already guessed these letters: "
+  bad_guesses.sort.uniq.each { |letter| print letter }
+
+  puts
   chosen_letters.each do |letter|
     if correct_guesses.include? letter
       print letter
@@ -45,6 +60,8 @@ until winner do
       print "_"
     end
   end
+  puts
+
   print "> "
   letter_guess = gets.chomp
 end
